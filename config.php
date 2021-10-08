b84b31d7a67b0b53e9d3d88f8e6e4d7283f89782
Gilmar Ribeiro <rdsgilmar81@gmail.com>
2021-10-08 10:13:11 -0300

    com erro

--- config.php
+++ config.php
@@ -7,12 +7,12 @@ $dbName = 'remanejamento_interno_escola1';
 
 $conexao = new mysqli($dbHos,$dbUsername,$dbPassword,$dbName);
 
-if ($conexao -> connect_errno)
-{
-    echo "erro";
-}
-else{
-    echo "Conexão efetuada com sucesso";
-}
+//if ($conexao -> connect_errno)
+//{
+//    echo "erro";
+//}
+//else{
+ //   echo "Conexão efetuada com sucesso";
+//}
 
 ?>
