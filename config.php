411f804480a9e47aeb15b7276a03651386c55c63
Gilmar Ribeiro <rdsgilmar81@gmail.com>
2021-10-08 09:57:28 -0300

    inserindo php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbHost ='LocalHost';
+$dbUsername = 'root';
+$dbPassword ='';
+$dbName = 'remanejamento_interno_escola1';
+
+$conexao = new mysqli($dbHos,$dbUsername,$dbPassword,$dbName);
+
+if ($conexao -> connect_errno)
+{
+    echo "erro";
+}else{
+    echo "Conexão efetuada com sucesso"
+}
+
+
+
+?>
